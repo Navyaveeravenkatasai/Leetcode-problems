9679134c738fd63003df3b1d25d0294e4bc7749c
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-01-03 15:04:26 +0530

    Time: 255 ms (0%), Space: 0B (0%) - LeetHub

---
+++ 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
@@ -0,0 +1,5 @@
+select  user_id,count(user_id) as prompt_count , round(avg(tokens),2) as avg_tokens
+from prompts
+group by user_id
+having count(user_id) >=3 and max(tokens) > avg(tokens)
+order by avg_tokens desc, user_id asc
