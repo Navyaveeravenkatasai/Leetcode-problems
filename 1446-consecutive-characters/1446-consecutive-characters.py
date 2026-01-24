e835d9e63f19ac992efe402bdf4b749d820762d0
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-01-24 20:53:35 +0530

    Time: 3 ms (90%), Space: 19.3 MB (16.51%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.py
+++ 1446-consecutive-characters/1446-consecutive-characters.py
@@ -3,11 +3,12 @@ class Solution:
         curr_len = 1
         max_len = 1
 
-        for i in range(1, len(s)):
+        for i in range(1,len(s)):
             if s[i] == s[i-1]:
                 curr_len += 1
                 max_len = max(max_len, curr_len)
             else:
                 curr_len = 1
-
         return max_len
+
+
