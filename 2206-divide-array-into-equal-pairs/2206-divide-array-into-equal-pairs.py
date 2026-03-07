9436c03a67ce1ee2d898859b18ac0c046cad29d9
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-03-07 21:36:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,13 +1,9 @@
-
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        freq = {}
-
+        freq={}
         for ch in nums:
-            freq[ch] = freq.get(ch, 0) + 1
-
+            freq[ch] = freq.get(ch,0) + 1
         for i in freq.values():
-            if i % 2 != 0:   # if any frequency is odd
+            if i % 2 != 0:
                 return False
-
         return True
