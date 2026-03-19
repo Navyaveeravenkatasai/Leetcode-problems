a0abfdc1d18cf419d1a571ac002f1588227147d6
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-03-19 15:19:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,9 +1,16 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        if word.isupper():
+        count=0
+        for ch in word:
+            if ord('A') <= ord(ch) <= ord('Z'):
+                count+=1
+        
+        if count == len(word):
             return True
-        if word.islower():
+        
+        if count == 0:
             return True
-        if word.istitle():
+        
+        if count == 1 and ord('A') <= word[0] <= ord('Z'):
             return True
         return False
