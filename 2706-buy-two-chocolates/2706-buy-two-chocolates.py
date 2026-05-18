9ad7fcf242c2007a2280a27f148bdb1fd8ab066c
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-05-18 12:31:00 +0530

    Time: 3 ms (34.69%), Space: 19.1 MB (89.54%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,9 +1,7 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         prices.sort()
-
-        for ch in range(0,len(prices)):
-            total = prices[0] + prices[1]
+        total = prices[0] + prices[1]
         
         if total == money:
             return 0
