c45df23d2745168cab910181417d6bb6d024b3d6
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-05-30 15:55:04 +0530

    Time: 1 ms (35.7%), Space: 19.1 MB (86.03%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
@@ -1,15 +1,11 @@
 class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
         first = 0
+        double = 0
         for ch in range(len(nums)):
             if nums[ch] < 10:
                 first += nums[ch]
-        second = 0
+            else:
+                double += nums[ch]
 
-        for ch in range(len(nums)):
-            if nums[ch] >= 10:
-                second += nums[ch]
-
-        if first > second or second > first:
-            return True
-        return False
+        return first != double
