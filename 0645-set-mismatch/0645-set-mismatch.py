1dca3aabe6752ecf6de59d9cda27c595f38a3c49
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-06-24 21:15:10 +0530

    Time: 4155 ms (9.28%), Space: 20.6 MB (54.67%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        res = []
+        n = len(nums)
+
+        duplicate = -1
+        missing = -1
+
+        for ch in range(1, n + 1):
+            res.append(ch)
+
+        for num in nums:
+            if nums.count(num) == 2:
+                duplicate = num
+                break
+
+        for num in res:
+            if num not in nums:
+                missing = num
+                break
+
+        return [duplicate, missing]
