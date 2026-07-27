8c0e391cf08c5762756c283ed5f2852c08f26670
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-07-27 21:56:49 +0530

    Time: 619 ms (23.04%), Space: 0B (100%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select distinct teacher_id, count(distinct subject_id) as cnt 
+-- Write your PostgreSQL query statement below
+select teacher_id , count(distinct(subject_id)) as cnt 
 from Teacher
 group by teacher_id
