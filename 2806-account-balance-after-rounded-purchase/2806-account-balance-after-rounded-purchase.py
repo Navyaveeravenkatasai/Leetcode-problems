060ddb2f0f3139f31bb79191b87f8ab8e9eb7805
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-08-04 21:17:03 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.15%) - LeetHub

---
+++ 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.py
@@ -0,0 +1,8 @@
+class Solution:
+    def accountBalanceAfterPurchase(self, purchaseAmount: int) -> int:
+
+        digit = purchaseAmount % 10
+        if digit >= 5:
+            return 100 - (purchaseAmount + (10 - digit))
+        else:
+            return 100 - (purchaseAmount - digit)
