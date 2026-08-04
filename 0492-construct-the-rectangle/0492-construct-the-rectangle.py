bbbb6d62bfaaa1cc32d3739119506b7b4ea818af
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-08-04 19:57:23 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.3%) - LeetHub

---
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.py
@@ -0,0 +1,7 @@
+from math import sqrt
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        w = int(sqrt(area))
+        while area % w:
+            w -= 1
+        return [area//w, w]
