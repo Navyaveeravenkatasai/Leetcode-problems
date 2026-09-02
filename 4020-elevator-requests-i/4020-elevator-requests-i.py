424b15beb2b45b5f7d340b8da817a80dd6106c3e
Navyaveeravenkatasai <161604081+Navyaveeravenkatasai@users.noreply.github.com>
2026-09-02 07:00:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.66%) - LeetHub

--- 4020-elevator-requests-i/4020-elevator-requests-i.py
+++ 4020-elevator-requests-i/4020-elevator-requests-i.py
@@ -1,16 +1,6 @@
 class Solution:
     def elevatorRequests(self, n: int, requests: list[int]) -> int:
-        count = requests[0]
-
+        res = 0
         for i in range(1,len(requests)):
-            res =0
-            if requests[i] < requests[i-1]:
-                res = requests[i-1] - requests[i]
-                count += res
-            elif requests[i] > requests[i-1]:
-                res = requests[i] - requests[i-1]
-                count += res
-            elif requests[i] == requests[i-1]:
-                continue
-        
-        return count
+            res += abs(requests[i-1] - requests[i])
+        return res + requests[0]
